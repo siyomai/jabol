0d7e440c25c04e582e8ae36836889b6aa4ffc7e0
Arnaldo Tayao Jr <arnaldotayaojr@gmail.com>
2025-06-04 18:39:52 +0800

    Removes boilerplate test assertions

--- test/jabol_test.exs
+++ test/jabol_test.exs
@@ -2,10 +2,6 @@ defmodule JabolTest do
   use ExUnit.Case
   doctest Jabol
 
-  test "greets the world" do
-    assert Jabol.hello() == :world
-  end
-
   test "project modules are properly defined" do
     assert Code.ensure_loaded?(Jabol.Schema)
     assert Code.ensure_loaded?(Jabol.Repo)
